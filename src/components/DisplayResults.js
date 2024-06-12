f1ae0a6c2b7de5acedfc612429e92f761f2c31d6
Rapil <120554681+Rapil3090@users.noreply.github.com>
2024-06-12 11:59:54 +0900

    결과값 수정

--- src/components/DisplayResults.js
+++ src/components/DisplayResults.js
@@ -20,15 +20,30 @@ function DisplayResults() {
 
   return (
     <div>
-      <h2>Display Non-matching Nh Data</h2>
+      <h2>매칭하기</h2>
       <input type="text" value={month} onChange={handleMonthChange} placeholder="Month" />
       <button onClick={fetchResults}>Fetch Results</button>
       {results.length > 0 && (
-        <ul>
-          {results.map((result) => (
-            <li key={result.id}>{result.name}</li>
-          ))}
-        </ul>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>securities Number</th>
+              <th>Target Number</th>
+              <th>Accident Number</th>
+            </tr>
+          </thead>
+          <tbody>
+            {results.map((result) => (
+              <tr key={result.id}>
+                <td>{result.name}</td>
+                <td>{result.securitiesNumber}</td>
+                <td>{result.targetNumber}</td>
+                <td>{result.accidentNumber}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       )}
     </div>
   );
