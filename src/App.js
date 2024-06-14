b184cbe89a30bd18a0fdc742e6749457844f9586
rapil <sjw3090@gmail.com>
2024-06-14 15:12:42 +0900

    네비 바 css 수정

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import UploadCropPage from './pages/UploadCropPage';
 import UploadNhPage from './pages/UploadNhPage';
 import DisplayResultsPage from './pages/DisplayResultsPage';
 import DisplayNhDataPage from './pages/DisplayNhDataPage';
-import './App.css'; // Import the CSS file
+import './App.css';
 
 function App() {
   return (
@@ -14,10 +14,15 @@ function App() {
         <h1>엑셀 비교하기</h1>
         <nav>
           <ul>
-            <li><Link to="/upload-crop">위임내역 업로드</Link></li>
-            <li><Link to="/upload-nh">농손 업로드</Link></li>
-            <li><Link to="/display-results">결과 조회</Link></li>
-            <li><Link to="/display-nh-data">데이터 비교하기</Link></li>
+            <li>
+              <span>엑셀</span>
+              <div className="dropdown">
+                <Link to="/upload-crop">위임내역 업로드</Link>
+                <Link to="/upload-nh">농손 업로드</Link>
+                <Link to="/display-results">결과 조회</Link>
+                <Link to="/display-nh-data">데이터 비교하기</Link>
+              </div>
+              </li>
           </ul>
         </nav>
         <Routes>
